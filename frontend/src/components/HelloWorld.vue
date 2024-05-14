1ac0fb431638261261d5f02d3130d511a831592f
Saroz <31454493+spsaroj@users.noreply.github.com>
2024-05-14 13:23:43 -0500

    plain todo task app. no backend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,41 +1,51 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div>
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+    <button @click="addTodo">Add Task</button>
+    
+
+  <div v-if="loading">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          {{ item.task_name }}
+          <button @click="deleteTodo(index)">Delete</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      newTodo: '',
+      loading: true,
+      items: []
+    };
+  },
+  mounted() {
+        axios.get('http://127.0.0.1:8080/tasks/')
+        .then(response=>(this.item = response))
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({ text: this.newTodo });
+        this.newTodo = '';
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
 </style>
